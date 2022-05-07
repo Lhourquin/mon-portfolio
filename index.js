255e787c451c746d6983f787529e3c795029b97c
lhourquin <lhourquin@gmail.com>
2022-05-07 09:57:40 +0200

    annimation rotate

---
+++ index.js
@@ -0,0 +1,22 @@
+console.clear();
+let angle = 360/6;
+let finalAngle = -360;
+
+const circleContainer = document.querySelectorAll(".network");
+
+
+
+circleContainer.forEach(circle =>{
+  
+  
+  finalAngle -=90;
+  
+  circle.style.transform = `rotate(${angle}deg)`;
+  circle.style.setProperty("--start-angle-of-rotation", `-${angle}deg`);
+  circle.style.setProperty("--end-angle-of-rotation", `${finalAngle}deg`);
+  
+  
+  angle += 90;
+  
+});
+
